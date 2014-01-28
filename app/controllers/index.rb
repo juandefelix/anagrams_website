7ee8f949f5833188bc18c9bc6c66dac921d5483d
Apprentice <apprentice@dbc-chicago-7.local>
2014-01-27 18:37:20 -0600

    returning strings instead that an array

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,11 +2,13 @@ get '/' do
   @anagram = params[:word]
   # Look in app/views/index.erb
   erb :index
+  # params
 end
 
 
 post '/word' do
     response = Word.anagrams(params[:user_input])
+    response = response.join(", ").upcase
 
     redirect to ("/?word=#{response}")
 
